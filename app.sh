70c98a967f59eb24365d30ecaea1c1a5730b9bb0
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2021-05-29 10:59:19 +0530

    chore(app.sh): Add usage

--- app.sh
+++ app.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+if [[ -z $1 ]]; then
+	echo "Usage: $0 <markdown file> <template file>"
+	exit
+fi
+
 TITLE=$(sed 's/# //; 1q' $1)
 
 TEMP="${2:-template.html}"
 
 if [[ ! -e $TEMP ]]; then
 	echo "$TEMP does not exist. Getting it from github..."
-	curl "https://raw.githubusercontent.com/sabyabhoi/abw/main/static/template.html"
+	curl "https://raw.githubusercontent.com/sabyabhoi/abw/main/template.html"
 fi
 
 sed -e 's|{0}|'"$TITLE"'|g' $TEMP | head -n -2
