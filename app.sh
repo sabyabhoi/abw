f10e45922206480177c5d3af009f85a700ad864f
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2021-05-29 09:55:56 +0530

    refactor(app.sh): Make $TEMP variable for template

--- app.sh
+++ app.sh
@@ -2,7 +2,14 @@
 
 TITLE=$(sed 's/# //; 1q' $1)
 
-sed -e 's|{0}|'"$TITLE"'|g' ./static/template.html | head -n -2
+TEMP="${2:-template.html}"
+
+if [[ ! -e $TEMP ]]; then
+	echo "$TEMP does not exist. Getting it from github..."
+	curl "https://raw.githubusercontent.com/sabyabhoi/abw/main/static/template.html"
+fi
+
+sed -e 's|{0}|'"$TITLE"'|g' $TEMP | head -n -2
 
 tail -n +2 $1 | markdown
 
