bc9c5b28f80b2f9befee65c822ff838ab5159f72
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2021-06-03 10:10:31 +0530

    fix(app.sh): Remove a nasty bug that print the output twice

--- app.sh
+++ app.sh
@@ -2,6 +2,7 @@
 
 if [[ -z $1 ]]; then
 	echo "Usage: $0 <markdown file> <template file> [--css] [--html]"
+	exit
 fi
 
 for arg in "$@"; do
@@ -17,27 +18,21 @@ for arg in "$@"; do
 			echo "Fetching html file from github..."
 			curl "https://raw.githubusercontent.com/sabyabhoi/abw/main/template.html" -o template.html
 			;;
-		*)
-			if [[ -z $1 ]]; then
-				echo "Usage: $0 <markdown file> <template file> [--css]"
-				exit
-			fi
-
-			TITLE=$(sed 's/# //; 1q' $1)
-
-			TEMP="${2:-template.html}"
-
-			if [[ ! -e $TEMP ]]; then
-				echo "$TEMP does not exist. Getting it from github..."
-				curl "https://raw.githubusercontent.com/sabyabhoi/abw/main/template.html" -o template.html
-			fi
-
-			sed -e 's|{0}|'"$TITLE"'|g' $TEMP | head -n -2
-
-			tail -n +2 $1 | markdown
-
-			echo "</body></html>"
-			;;
 	esac
 done
 
+
+TITLE=$(sed 's/# //; 1q' $1)
+
+TEMP="${2:-template.html}"
+
+if [[ ! -e $TEMP ]]; then
+	echo "$TEMP does not exist. Getting it from github..."
+	curl "https://raw.githubusercontent.com/sabyabhoi/abw/main/template.html" -o template.html
+fi
+
+sed -e 's|{0}|'"$TITLE"'|g' $TEMP | head -n -2
+
+tail -n +2 $1 | markdown
+
+echo "</body></html>"
